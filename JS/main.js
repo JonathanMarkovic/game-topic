a9d7e488bfd5aa7131e20fce77ed59b6835993d1
JonathanMarkovic <jonathan.markovic@outlook.com>
2025-04-07 20:45:46 -0400

    Some work on main.js to prepare for implementation of dynamic lists
    Added some function to be altered at a later date to implement different features of the product page

---
+++ JS/main.js
@@ -0,0 +1,39 @@
+//Making sure the HTML is all loaded
+document.addEventListener('DOMContentLoaded', initApp);
+
+function initApp() {
+    console.log("Initializing webpage");
+
+
+    //Create list of games here
+    const games = [];
+
+    
+    displayGames(games);
+}
+
+function displayGames(games) {
+    console.log("Displaying Games");
+
+    let gameList = "<ol>";
+    games.array.forEach(game => {
+        console.log(game);
+        gameList += `<li>${game.displayInfo()}</li>`
+    });
+    gameList += '</ol>';
+    const gamePlaceholder = document.getElementById("gameList");
+    gamePlaceholder.innerHTML = gameList;
+}
+
+class Game {
+    constructor(title, genre, price, rating) {
+        this.title = title;
+        this.genre = genre;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    displayInfo() {
+        return `${title}, ${genre}, ${price}, ${rating}`;
+    }
+}
